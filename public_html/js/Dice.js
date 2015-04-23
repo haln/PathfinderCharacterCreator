1a12173de702517bf85a5d22492e66e7cf58c684
JustinWalk <justinwalk@live.com>
2015-04-23 14:21:29 -0700

    moved into js

---
+++ public_html/js/Dice.js
@@ -0,0 +1,71 @@
+function rollD2(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 2 + 1);
+    }
+	return result;
+}
+
+function rollD3(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 3 + 1);
+    }
+	return result;
+}
+
+function rollD4(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 4 + 1);
+    }
+	return result;
+}
+
+function rollD6(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 6 + 1);
+    }
+	return result;
+}
+
+function rollD8(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 8 + 1);
+    }
+	return result;
+}
+
+function rollD10(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 10 + 1);
+    }
+	return result;
+}
+
+function rollD12(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 12 + 1);
+    }
+	return result;
+}
+
+function rollD20(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 20 + 1);
+    }
+	return result;
+}
+
+function rollD100(numRolls) {
+    result = 0;
+    for (i = 0; i < numRolls; i++) {
+        result += Math.floor(Math.random() * 100 + 1);
+    }
+	return result;
+}
