35dbbc2e228ff6b5f4c3bb961f6d2a623ecad315
JustinWalk <justinwalk@live.com>
2015-04-28 12:39:31 -0700

    xml nonsense

--- public_html/js/AttributeFunctions.js
+++ public_html/js/AttributeFunctions.js
@@ -8,34 +8,34 @@ function getImage(num) {
     return image;
 }
 
-function rollStat(){
+function rollStat() {
     var nums = [];
     var lowest = 6;
     var removeIndex;
-    for (index = 0; index < 4; index++){
+    for (index = 0; index < 4; index++) {
         nums[index] = rollD6(1);
-        if (nums[index] <= lowest){
+        if (nums[index] <= lowest) {
             lowest = nums[index];
             removeIndex = index;
         }
     }
-    
+
     nums.splice(removeIndex, 1);
     var result = 0;
-    for (i = 0; i < nums.length; i++){
+    for (i = 0; i < nums.length; i++) {
         result += nums[i];
     }
     return result;
 }
 
-function resetStats(){
+function resetStats() {
     resetContainer("strCon");
     resetContainer("dexCon");
     resetContainer("conCon");
     resetContainer("intCon");
     resetContainer("wisCon");
     resetContainer("chaCon");
-    
+
     resetDiceRoll("stat1");
     resetDiceRoll("stat2");
     resetDiceRoll("stat3");
@@ -44,7 +44,7 @@ function resetStats(){
     resetDiceRoll("stat6");
 }
 
-function resetContainer(id){
+function resetContainer(id) {
     document.getElementById(id).setAttribute("src", "Images/Dice/container.png");
     document.getElementById(id).setAttribute("ondragover", "allowDrop(event)");
     document.getElementById(id).setAttribute("draggable", "false");
@@ -52,7 +52,7 @@ function resetContainer(id){
     document.getElementById(id).setAttribute("ondrop", "drop(event)");
 }
 
-function resetDiceRoll(id){
+function resetDiceRoll(id) {
     document.getElementById(id).setAttribute("ondragover", "");
     document.getElementById(id).setAttribute("draggable", "true");
     document.getElementById(id).setAttribute("ondragstart", "drag(event)");
@@ -61,36 +61,36 @@ function resetDiceRoll(id){
 
 function rollCharacter() {
     resetStats();
-    
+
     var roll1 = document.getElementById("stat1");
     roll1.setAttribute("src", getImage(rollStat()));
-    
+
     var roll2 = document.getElementById("stat2");
     roll2.setAttribute("src", getImage(rollStat()));
-    
+
     var roll3 = document.getElementById("stat3");
     roll3.setAttribute("src", getImage(rollStat()));
-    
+
     var roll4 = document.getElementById("stat4");
     roll4.setAttribute("src", getImage(rollStat()));
-    
+
     var roll5 = document.getElementById("stat5");
     roll5.setAttribute("src", getImage(rollStat()));
-    
+
     var roll6 = document.getElementById("stat6");
     roll6.setAttribute("src", getImage(rollStat()));
 }
 
-function allowDrop(ev){
+function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function drag(ev){
+function drag(ev) {
     ev.dataTransfer.setData("id", ev.target.id);
     ev.dataTransfer.setData("image", ev.target.src);
 }
 
-function drop(ev){
+function drop(ev) {
     var data = ev.dataTransfer.getData("image");
     var droppedItem = document.getElementById(ev.dataTransfer.getData("id"));
     ev.target.setAttribute("src", data);
@@ -98,12 +98,22 @@ function drop(ev){
     ev.target.setAttribute("draggable", "true");
     ev.target.setAttribute("ondragstart", "drag(event)");
     ev.target.setAttribute("ondrop", "");
-    
+
     droppedItem.setAttribute("src", "Images/Dice/container.png");
     droppedItem.setAttribute("ondragover", "allowDrop(event)");
     droppedItem.setAttribute("draggable", "false");
     droppedItem.setAttribute("ondragstart", "");
     droppedItem.setAttribute("ondrop", "drop(event)");
-    
+
     ev.preventDefault();
 }
+
+function getXML(type) {
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", "XML/pathfinderXML.xml", false);
+        xmlhttp.send();
+        xmlDoc = xmlhttp.responseXML;
+        console.log(xmlDoc);
+        var description = xmlDoc.getElementsByTagName(type)[0].childNodes[0].nodeValue;
+        document.getElementById("txt_description").setAttribute("value", description);
+    }
