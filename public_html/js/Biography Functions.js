ac5f3f3954dec263f13b1e213aa52c48bb3d9ebf
JustinWalk <justinwalk@live.com>
2015-05-04 13:18:30 -0700

    stuff

---
+++ public_html/js/Biography Functions.js
@@ -0,0 +1,65 @@
+function getBio() {
+    if ((document.getElementById('selected_race').getAttribute("value")) == "Half-Elf") {
+        document.getElementById('favored_class2').removeAttribute("hidden");
+    } else {
+        document.getElementById('favored_class2').setAttribute("hidden", "hidden");
+    }
+    getAlignments(document.getElementById(selected_class).getAttribute("value"));
+}
+
+function getAlignments(classType){
+    switch(classType){
+        case "Barbarian" :
+            document.getElementById(alignSelect).innerHTML =
+                "<option>Neutral Good</option>\n\
+                 <option>Chaotic Good</option>\n\
+                 <option>True Neutral</option>\n\
+                 <option>Chaotic Neutral</option>\n\
+                 <option>Neutral Evil</option>\n\
+                 <option>Chaotic Evil</option>\n\
+                ";
+            break;
+            
+        case "Cleric" :
+            break;
+            
+        case "Druid" :
+            document.getElementById(alignSelect).innerHTML =
+                "<option>Neutral Good</option>\n\
+                 <option>Lawful Neutral</option>\n\
+                 <option>True Neutral</option>\n\
+                 <option>Chaotic Neutral</option>\n\
+                 <option>Neutral Evil</option>\n\
+                ";
+            break;
+            
+        case "Monk" :
+            document.getElementById(alignSelect).innerHTML =
+                "<option>Lawful Good</option>\n\
+                 <option>Lawful Neutral</option>\n\
+                 <option>Lawful Evil</option>\n\
+                ";
+            break;
+            
+        case "Paladin" :
+            document.getElementById(alignSelect).innerHTML =
+                "<option>Lawful Good</option>\n\
+                ";
+            break;
+            
+        default :
+            document.getElementById(alignSelect).innerHTML =
+                "<option>Lawful Good</option>\n\
+                 <option>Neutral Good</option>\n\
+                 <option>Chaotic Good</option>\n\
+                 <option>Lawful Neutral</option>\n\
+                 <option>True Neutral</option>\n\
+                 <option>Chaotic Neutral</option>\n\
+                 <option>Lawful Evil</option>\n\
+                 <option>Neutral Evil</option>\n\
+                 <option>Chaotic Evil</option>\n\
+                ";
+            break;
+    }
+}
+
