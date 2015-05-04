a8809d1fd4676d382747df8d177676ba869a3bb3
JustinWalk <justinwalk@live.com>
2015-05-04 12:44:45 -0700

    MOAR STATS

--- public_html/js/AttributeFunctions.js
+++ public_html/js/AttributeFunctions.js
@@ -45,6 +45,7 @@ function resetContainer(id) {
     document.getElementById(id).setAttribute("draggable", "false");
     document.getElementById(id).setAttribute("ondragstart", "");
     document.getElementById(id).setAttribute("ondrop", "drop(event)");
+    document.getElementById(id).setAttribute("alt", "container");
 }
 
 function resetDiceRoll(id) {
@@ -52,28 +53,41 @@ function resetDiceRoll(id) {
     document.getElementById(id).setAttribute("draggable", "true");
     document.getElementById(id).setAttribute("ondragstart", "drag(event)");
     document.getElementById(id).setAttribute("ondrop", "");
+    document.getElementById(id).setAttribute("alt", "");
 }
 
 function rollCharacter() {
     resetStats();
 
     var roll1 = document.getElementById("stat1");
-    roll1.setAttribute("src", getImage(rollStat()));
+    var rollValue1 = rollStat();
+    roll1.setAttribute("src", getImage(rollValue1));
+    roll1.setAttribute("alt", rollValue1);
 
     var roll2 = document.getElementById("stat2");
-    roll2.setAttribute("src", getImage(rollStat()));
+    var rollValue2 = rollStat();
+    roll2.setAttribute("src", getImage(rollValue2));
+    roll2.setAttribute("alt", rollValue2);
 
     var roll3 = document.getElementById("stat3");
-    roll3.setAttribute("src", getImage(rollStat()));
+    var rollValue3 = rollStat();
+    roll3.setAttribute("src", getImage(rollValue3));
+    roll3.setAttribute("alt", rollValue3);
 
     var roll4 = document.getElementById("stat4");
-    roll4.setAttribute("src", getImage(rollStat()));
+    var rollValue4 = rollStat();
+    roll4.setAttribute("src", getImage(rollValue4));
+    roll4.setAttribute("alt", rollValue4);
 
     var roll5 = document.getElementById("stat5");
-    roll5.setAttribute("src", getImage(rollStat()));
+    var rollValue5 = rollStat();
+    roll5.setAttribute("src", getImage(rollValue5));
+    roll5.setAttribute("alt", rollValue5);
 
     var roll6 = document.getElementById("stat6");
-    roll6.setAttribute("src", getImage(rollStat()));
+    var rollValue6 = rollStat();
+    roll6.setAttribute("src", getImage(rollValue6));
+    roll6.setAttribute("alt", rollValue6);
 }
 
 function allowDrop(ev) {
@@ -83,22 +97,49 @@ function allowDrop(ev) {
 function drag(ev) {
     ev.dataTransfer.setData("id", ev.target.id);
     ev.dataTransfer.setData("image", ev.target.src);
+    ev.dataTransfer.setData("alt", ev.target.alt);
 }
 
 function drop(ev) {
     var data = ev.dataTransfer.getData("image");
+    var altValue = ev.dataTransfer.getData("alt");
     var droppedItem = document.getElementById(ev.dataTransfer.getData("id"));
     ev.target.setAttribute("src", data);
     ev.target.setAttribute("ondragover", "");
     ev.target.setAttribute("draggable", "true");
     ev.target.setAttribute("ondragstart", "drag(event)");
     ev.target.setAttribute("ondrop", "");
+    ev.target.setAttribute("alt", altValue);
 
     droppedItem.setAttribute("src", "Images/Dice/container.png");
     droppedItem.setAttribute("ondragover", "allowDrop(event)");
     droppedItem.setAttribute("draggable", "false");
     droppedItem.setAttribute("ondragstart", "");
     droppedItem.setAttribute("ondrop", "drop(event)");
+    droppedItem.setAttribute("alt", "container");
 
     ev.preventDefault();
+}
+
+function assignStats(){
+    getHref();
+    var strength = document.getElementById("strCon").getAttribute("alt");
+    var dexterity = document.getElementById("dexCon").getAttribute("alt");
+    var constitution = document.getElementById("conCon").getAttribute("alt");
+    var intelligence = document.getElementById("intCon").getAttribute("alt");
+    var wisdom = document.getElementById("wisCon").getAttribute("alt");
+    var charisma = document.getElementById("chaCon").getAttribute("alt");
+}
+
+function getHref(){
+    if (document.getElementById("strCon").getAttribute("alt") != "container" &&
+        document.getElementById("dexCon").getAttribute("alt") != "container" &&
+        document.getElementById("conCon").getAttribute("alt") != "container" &&
+        document.getElementById("intCon").getAttribute("alt") != "container" &&
+        document.getElementById("wisCon").getAttribute("alt") != "container" &&
+        document.getElementById("chaCon").getAttribute("alt") != "container") {
+        document.getElementById("statNext").setAttribute("href", "#race");
+    } else {
+        document.getElementById("statNext").setAttribute("href", "#");
+    }
 }
