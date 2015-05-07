ab801184bccf7591f6934e13e73fbab5b49762a4
Casper Co <haln22111@gmail.com>
2015-05-07 13:04:33 -0700

    Database Connection Stuff
    Added some get work.

--- public_html/db_conn.php
+++ public_html/db_conn.php
@@ -2,19 +2,42 @@
 $dsn = 'mysql:host=p3nlmysql61plsk.secureserver.net;port=3306;dbname=pathfinder';
 $username = 'pathfinder';
 $password = 'project2015';
-if(isset($_POST["query"])){
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-        die();
+$methodType = $_SERVER['REQUEST_METHOD'];
+if($methodType == 'GET'){
+    if(isset($_GET["query"])){
+        $query = $_GET["query"];
+        try{
+            $conn = new PDO($dsn, $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $statement = $conn->query($query);
+            die($statement->fetchAll());
+        } catch (PDOException $ex) {
+            error_log($e->getMessage());
+        }
     }
-    $query = $_POST["query"];
-    try{
-        $conn = new PDO($dsn, $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $statement = $conn->prepare($query);
-        die($statement->fetchAll());
-    } catch (PDOException $ex) {
-        error_log($e->getMessage());
+    else{
+        die("Error occured.");
+    }
+}
+if($methodType == 'POST'){
+    if(isset($_POST["query"])){
+        if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+            die();
+        }
+        $query = $_POST["query"];
+        try{
+            $conn = new PDO($dsn, $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $statement = $conn->query($query);
+            die($statement->fetchAll());
+        } catch (PDOException $ex) {
+            error_log($e->getMessage());
+        }
+    }
+    else{
+        die("Error occured.");
     }
 }
 ?>
