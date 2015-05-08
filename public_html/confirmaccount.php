2d369f52f1e3974700c1804c9e69c66f5aff6f33
Casper Co <haln22111@gmail.com>
2015-05-08 14:27:41 -0700

    Quick update
    Just changed the confirmaccount and login pages to conform with the new
    master header and footer files. Also finally got around to deleting the
    confusing confirm.html which was deprecated when I switched to using the
    confirmaccount.php file instead

--- public_html/confirmaccount.php
+++ public_html/confirmaccount.php
@@ -1,35 +1,5 @@
-<!DOCTYPE html>
-<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
-    <head>
-        <title>Pathfinder Character Creator - Account Creation Confirmation</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="css/animations.css">
-    </head>
-    <body>
-        <!-- Top Bar Menu -->
-        <form id="signin" action="index.html">
-            <input type="text" placeholder="Username">
-            <input type="password" placeholder="Password">
-            <input type="submit">
-        </form>
-        <a href='profile.html'>My Profile</a>
-        <!-- The Navigation Menu -->
-        <nav>
-            <div id="header">
-                <h1>Pathfinder Character Creator</h1>
-                <ul id="navigation">
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="characterWizard.html">Character Wizard</a></li>
-                    <li><a href="signup.html">Account Signup</a></li>
-                </ul>
-            </div>
-        </nav>
+<?php require 'php/header.php'; ?>
+
         <div id="profile" class="content">
             <h2>Account Signup</h2>
             <p>
@@ -84,5 +54,4 @@
             </p>
         </div>
         <script src="js/jquery-2.1.3.min.js"></script>
-    </body>
-</html>
+<?php require 'php/footer.php'; ?>
