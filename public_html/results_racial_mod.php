42941171518809ad6d218c71f0878013b304c15e
JustinWalk <justinwalk@live.com>
2015-05-11 15:17:44 -0700

    nonsense

--- public_html/results_racial_mod.php
+++ public_html/results_racial_mod.php
@@ -3,11 +3,10 @@ $dsn = 'mysql:host=p3nlmysql61plsk.secureserver.net;port=3306;dbname=pathfinder'
 $username = 'pathfinder';
 $password = 'project2015';
 $methodType = $_SERVER['REQUEST_METHOD'];
-$data = array("msg" => "$methodType");
 if($methodType === 'GET'){
     if(isset($_GET["race"])){
         if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-            die();
+          // die();
         }
 
         try{
@@ -15,11 +14,14 @@ if($methodType === 'GET'){
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             $race = ($_GET["race"]);
-            $statement = $conn->prepare('SELECT * FROM RACE WHERE RACE_NAME = :race');
+            $column = ($_GET["column"]);
+            $statement = $conn->prepare("SELECT * FROM RACE WHERE RACE_NAME = :race");
             $statement->execute(array(
                 ':race' => $race
             ));
-            die($statement->fetchAll());
+
+            $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($data, JSON_FORCE_OBJECT);
         } catch (PDOException $e) {
             error_log($e->getMessage());
             die('');
