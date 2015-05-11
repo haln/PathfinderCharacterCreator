582c6895db7cb70880cc1b4da456b1506dbb2e92
Casper Co <haln22111@gmail.com>
2015-05-11 15:48:53 -0700

    Some work on characters
    Getting ready for character sheets!

---
+++ public_html/php/characters.php
@@ -0,0 +1,34 @@
+<?php
+include 'db_login.php';
+$methodType = $_SERVER['REQUEST_METHOD'];
+if($methodType === 'POST'){
+    if(isset($_POST["char"])){
+        if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+            die();
+        }
+
+        $acc_userID = $_POST["user"];
+        
+        try{
+            $conn = new PDO($dsn, $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $statement = $conn->prepare('SELECT USER_CHAR.CHAR_ID, CHAR_NAME FROM USER_CHAR JOIN CHARACTERS WHERE USER_ID = :user AND CHARACTERS.CHAR_ID = USER_CHAR.CHAR_ID');
+            $statement->execute(array(
+                ':user' => $acc_userID
+            ));
+            $results = $statement->fetchAll();
+            if(empty($results)){
+                die(false);
+            }
+            echo json_encode($results);
+            die(); 
+            
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            die(false);
+        }
+
+    }
+}
+?>
