bb45738300e7ab63587d889aecd711a7f5183176
Casper Co <haln22111@gmail.com>
2015-05-12 16:06:33 -0700

    Change Password Functional
    The user can now change their password in their user profile. Fully
    functioning with validation and database update

--- public_html/js/profileManagement.js
+++ public_html/js/profileManagement.js
@@ -1,8 +1,24 @@
+$(document).ready(function(){
+    //Change the page to show the user's username
+    $username = localStorage.getItem("username");
+    if($username){
+        $("#user_name").html($username);
+    }
+});
+
 function changePassword(){
-    if($("#newPass").val() === $("confirmNewPass").val()){
-        $.post("changePassword.php", {password: $("#newPass").val(), userID: localStorage.getItem("userID")}).done(function(data){
-            $("#newPassLabel").html(data);
+    var newpass = $("#newPass").val();
+    var confirmnewpass = $("#confirmNewPass").val();
+    console.log("New pass: "+newpass);
+    console.log("Confirm: "+confirmnewpass);
+    //If the two passwords match, ask the database to change the user's password
+    if(newpass === confirmnewpass){
+        $.post("php/changePassword.php", {password: newpass, userID: localStorage.getItem("userID")}).done(function(data){
+            $("#newPassLabel").html("Password successfully changed");
+            return true;
         });
+        $("#newPassLabel").html("Unsuccessful attempt at changing password");
+        return false;
     }
 }
 
--- public_html/php/changePassword.php
+++ public_html/php/changePassword.php
@@ -1,5 +1,5 @@
 <?php
-require 'php/db_login.php';
+require 'db_login.php';
 $methodType = $_SERVER["REQUEST_METHOD"];
 if($methodType === "POST"){
     if(isset($_POST["password"]) && isset($_POST["userID"])){
@@ -8,22 +8,16 @@ if($methodType === "POST"){
         }
         $newpassword = $_POST["password"];
         $userID = $_POST["userID"];
-        
         try{
             $conn = new PDO($dsn, $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
             $statement = $conn->prepare('UPDATE USERS SET USER_PASS = :pass WHERE USER_ID = :user');
             $statement->execute(array(
                 ':user' => $userID,
                 ':pass' => $newpassword
             ));
-            $results = $statement->fetch(PDO::FETCH_ASSOC);
-            if(empty($results)){
-                die(false);
-            }
-            echo json_encode($results);
-            die(); 
+            echo "Password changed";
+            die(true); 
         } catch (PDOException $e) {
             error_log($e->getMessage());
             die(false);
