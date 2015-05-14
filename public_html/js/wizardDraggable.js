20e2f07e7748bbeedd30bf8bc8e3769df09f28d3
Casper Co <haln22111@gmail.com>
2015-05-14 15:49:31 -0700

    Maybe fixed mobile draggable?
    Tried to use some jquery stuff to make it draggable for mobile touch
    events.

---
+++ public_html/js/wizardDraggable.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $("#stat1").draggable();
+   $("#stat2").draggable();
+   $("#stat3").draggable();
+   $("#stat4").draggable();
+   $("#stat5").draggable();
+   $("#stat6").draggable();
+});
