41885a95a863880eb9dfd0e12c6e7ec1866bad50
JustinWalk <justinwalk@live.com>
2015-05-20 11:24:45 -0700

    svdsgf

--- public_html/php/results_save_character.php
+++ public_html/php/results_save_character.php
@@ -21,9 +21,10 @@ if($methodType == "POST"){
         try {
             $conn = new PDO($dsn, $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
             $rowStatement = $conn->query('SELECT COUNT(*) FROM CHARACTERS');
             $rowCount = $rowStatement->fetchColumn();
-
+            
             $strength     = $_POST["strength"];
             $dexterity    = $_POST["dexterity"];
             $constitution = $_POST["constitution"];
